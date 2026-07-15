d75d202a9fc3fd0dc62f804fba6ac19859680fc5
Xetch <poopyking482@gmail.com>
2026-07-15 12:47:13 +0800

    Create Stubs/stubs.c

---
+++ Clib/Stubs/stubs.c
@@ -0,0 +1,11 @@
+// stubs.c
+// Look at the compiler errors to see what names the headers are looking for
+int _isctype(int c, int type) {
+    // implement the bare-minimum logic your header expects
+    return (c >= '0' && c <= '9'); 
+}
+
+void __main() {
+    // MinGW/Win32 targets often look for an internal __main setup routine.
+    // You can leave this completely empty.
+}
