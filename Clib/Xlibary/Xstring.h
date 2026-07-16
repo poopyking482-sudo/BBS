ecec132cc14fcd9c8e71b8602968822d7c41f3dc
Xetch <poopyking482@gmail.com>
2026-07-16 21:32:28 +0800

    Create Xstring.h

---
+++ Clib/Xlibary/Xstring.h
@@ -0,0 +1,55 @@
+#ifndef XSTRING_H
+#define XSTRING_H
+
+// A simple integer-to-string converter for printing numbers
+static inline char* printX(int num, char* str, int base) {
+    int i = 0;
+    int is_negative = 0;
+
+    if (num == 0) {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+
+    if (num < 0 && base == 10) {
+        is_negative = 1;
+        num = -num;
+    }
+
+    while (num != 0) {
+        int rem = num % base;
+        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+        num = num / base;
+    }
+
+    if (is_negative) {
+        str[i++] = '-';
+    }
+
+    str[i] = '\0';
+
+    // Reverse the string
+    int start = 0;
+    int end = i - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+
+    return str;
+}
+
+// Get string length
+static inline int Xstrlen(const char* str) {
+    int len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+#endif // XSTRING_H
