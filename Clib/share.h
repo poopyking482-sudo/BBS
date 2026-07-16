64d69dd39d07c3b27edaec3d5236f335e47a7305
Xetch <poopyking482@gmail.com>
2026-07-16 16:19:05 +0800

    Create share.h

---
+++ Clib/share.h
@@ -0,0 +1,10 @@
+#ifndef _SHARE_H
+#define _SHARE_H
+
+#define SH_COMPAT 0x00
+#define SH_DENYRW 0x10  // Deny read/write access to others
+#define SH_DENYWR 0x20  // Deny write access to others
+#define SH_DENYRD 0x30  // Deny read access to others
+#define SH_DENYNO 0x40  // Deny nothing (allow full sharing)
+
+#endif
