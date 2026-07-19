1fdded2affd175863ca467ba6b9b915511adcc88
Xetch <poopyking482@gmail.com>
2026-07-19 19:31:31 +0800

    Update version.c

--- core/version.c
+++ core/version.c
@@ -2,7 +2,6 @@
 
 void version(int argc, char **argv) {
     io_print("BSS version 0.1\n"); 
-    io_print("Built by");
-    io_print("Xetch");
+    io_print("Built by Xetch");
     // contributors can build their name, only if they contributed to the kernel or anything
 }
