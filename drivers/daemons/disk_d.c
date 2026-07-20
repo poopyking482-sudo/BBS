487f1c6be8a893bc6cb34e8efb4e3b0f5b401a76
Xetch <poopyking482@gmail.com>
2026-07-20 17:46:36 +0800

    Create daemons/disk_d.c

---
+++ drivers/daemons/disk_d.c
@@ -0,0 +1,27 @@
+#include "sys_ipc.h"
+#include "init/sys_io.h"
+
+#define DISK_CMD_CREATE 0x01
+#define DISK_CMD_DELETE 0x02
+
+void disk_daemon_loop(void) {
+    message_t msg;
+
+    while (1) {
+        // Block/Listen for incoming IPC commands from any process
+        if (kipc_recv(IPC_ANY_SENDER, &msg) == 0) {
+            switch (msg.command) {
+                case DISK_CMD_CREATE:
+                    io_print("[disk_d] IPC Received: Creating file...\n");
+                    break;
+
+                case DISK_CMD_DELETE:
+                    io_print("[disk_d] IPC Received: Deleting file...\n");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
