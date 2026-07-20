013b90cd99962eaccc9bb6dea627ed65528fd8ad
Xetch <poopyking482@gmail.com>
2026-07-20 17:51:16 +0800

    Update sys_io.c

--- kernel/sys_io.c
+++ kernel/sys_io.c
@@ -1,4 +1,4 @@
-#include "kernel/include/k_ioports.h"
+#include "kernel/include/kio_ports.h"
 
 void outb(uint16_t port, uint8_t val) {
     __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
