4107847fcbb789fde5a249e354d72a47277c8612
Xetch <poopyking482@gmail.com>
2026-07-22 17:36:57 +0800

    Update coreutils.h

--- src/coreutils/include/coreutils.h
+++ src/coreutils/include/coreutils.h
@@ -1,10 +1,14 @@
 #ifndef COREUTILS_H
 #define COREUTILS_H
 
+// system utilities
 void core_echo(const char *args);
 void core_ls(const char *args);
 void core_cat(const char *filename);
 void core_grep(const char *pattern, const char *text);
-void core_chroot(const char *path);
+
+// password utilities 
+void core_sudo(const char *cmd, const char *arg, const char *password);
+void core_passwd(const char *target_user, const char *old_or_new_pass, const char *new_pass_if_root);
 
 #endif // COREUTILS_H
